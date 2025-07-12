2c7694550c074e8069e132d7b18a38466a260041
allan-bit-web <allankinuthia59@gmail.com>
2025-07-12 23:04:52 +0300

    my taskmanager containing admin and a developer

---
+++ server/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+
+// Connects to MongoDB using mongoose
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+        });
+        console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.error("MongoDb connection failed: ", error.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
