a6d901fbf61d47bd451b4bb17f6c88f0385aa929
allan-bit-web <allankinuthia59@gmail.com>
2025-07-13 12:41:51 +0300

    edited mongodb atlas connection

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB connected successfully!!!!!!!");
+        console.log("MongoDB connected successfully!");
     } catch (error) {
         console.error("MongoDb connection failed: ", error.message);
         process.exit(1);
