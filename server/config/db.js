f62a8d72cb170ca17184b69cc218bfb426d7150c
allan-bit-web <allankinuthia59@gmail.com>
2025-07-13 11:52:09 +0300

    updated the backend db.js file

--- server/config/db.js
+++ server/config/db.js
@@ -4,10 +4,7 @@ const mongoose = require("mongoose");
 // Connects to MongoDB using mongoose
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser:true,
-            useUnifiedTopology:true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected successfully");
     } catch (error) {
         console.error("MongoDb connection failed: ", error.message);
